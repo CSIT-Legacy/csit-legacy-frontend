a1da84f7c0aea9fb13685b5d174bc55d178cff7b
tolstovrob <tolstovrob@gmail.com>
2025-08-05 09:04:46 +0400

    feat(Button): allow all HTML props

--- src/shared/ui/button/ui/button.tsx
+++ src/shared/ui/button/ui/button.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { MouseEventHandler, ReactNode } from 'react';
+import { ButtonHTMLAttributes, MouseEventHandler, ReactNode } from 'react';
 import styles from './button.module.scss';
 
-export type ButtonProps = {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	onClick?: MouseEventHandler<HTMLButtonElement>;
 	children?: ReactNode;
 	disabled?: boolean;
 	variant?: 'default' | 'primary' | 'error' | 'success' | 'info' | 'warning';
 	size?: 'md' | 'sm' | 'lg' | 'icon';
-};
+}
 
 export function Button(props: ButtonProps) {
 	return (
@@ -20,7 +20,8 @@ export function Button(props: ButtonProps) {
         ${styles[props.size ?? 'md']}
       `}
 			disabled={props.disabled}
-			onClick={props.onClick ?? (() => {})}>
+			onClick={props.onClick ?? (() => {})}
+			{...props}>
 			{props.children}
 		</button>
 	);
