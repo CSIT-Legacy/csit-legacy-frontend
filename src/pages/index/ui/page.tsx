cc2d425a82f96563ec4f8c4cb9726af9bac6e4ad
aragami3070 <smirnov17612@gmail.com>
2025-08-14 02:23:51 +0400

    chore(page): remove useless mock usage

--- src/pages/index/ui/page.tsx
+++ src/pages/index/ui/page.tsx
@@ -1,17 +1,7 @@
-import { Button } from '@/shared/ui/button';
-import { RadioButton } from '@/shared/ui/radio-button';
-
 export function Page() {
 	return (
 		<div>
 			<h1>Hello, World! </h1>
-			<Button variant='info'>Aboba</Button>
-			<RadioButton variant='default'>Abobus</RadioButton>
-			<RadioButton variant='primary'>Abobus</RadioButton>
-			<RadioButton variant='error'>Abobus</RadioButton>
-			<RadioButton variant='success'>Abobus</RadioButton>
-			<RadioButton variant='info'>Abobus</RadioButton>
-			<RadioButton variant='warning'>Abobus</RadioButton>
 		</div>
 	);
 }
