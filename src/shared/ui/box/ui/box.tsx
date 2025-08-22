780c92102474ea32aa4d83b431b6eb524259deaf
tolstovrob <tolstovrob@gmail.com>
2025-08-22 23:25:02 +0400

    feat(shared/ui): add box component

---
+++ src/shared/ui/box/ui/box.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { ReactNode } from 'react';
+import styles from './box.module.scss';
+
+export interface BoxProps {
+	children?: ReactNode;
+	variant?: 'default' | 'primary' | 'error' | 'success' | 'info' | 'warning';
+	customClassName?: string
+  name?: string
+}
+
+export function Box({ children, variant, customClassName, name }: BoxProps) {
+	return (
+		<div className={`${styles.box} ${styles[variant ?? 'default']} ${customClassName ?? ''}`}>
+			{name && <label>{name}</label>}
+			{children}
+		</div>
+	);
+}
